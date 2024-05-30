5ab5297607e7e18e3fb18132e173bee8c27c7ef3
Guy Levy <guy.levy01@post.runi.ac.il>
2024-05-31 00:41:35 +0300

    logical check first letter + check existence in DB

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+from flask_cors import CORS
+import logging
 
 app = Flask(__name__)
+CORS(app)
+
+client = MongoClient('mongodb+srv://guylevy210:leguyvy210@clustertest.hkcqtoo.mongodb.net/')
+db = client['CountryCity']
+collection = db['country']
+
+logging.basicConfig(level=logging.DEBUG)
+
+@app.route('/checkvalid', methods=['GET'])
+def check_valid():
+
+    collection_name = request.args.get('collection')
+    field = request.args.get('field')
+    value = request.args.get('value')
+    
+    logging.debug(f"Collection: {collection_name}, Field: {field}, Value: {value}")
+
+    if not all([collection_name, field, value]):
+        return jsonify({'error': 'Missing data in request'}), 400
+    
+    collection = db[collection_name]
+    query = {field : value}
+    result = collection.find_one(query)
+    
+    if result:
+        return jsonify({'valid': True})
+    else:
+        return jsonify({'valid': False})
+
 
 @app.route('/')
 def home():
-   return "Welcome to the Home Page!"
+   return render_template('play.html')
 
 @app.route('/play')
 def play():
